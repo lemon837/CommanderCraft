5a94594170f79ec5f9cab6f215d89736d33d683e
lemon837 <105208298+lemon837@users.noreply.github.com>
2023-11-06 15:22:12 +0800

    Patch 4

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ A multiuse EDH deck analyser
 import json
 import math
 import mtg_parser
+import os.path
 from collections import Counter
-from multiprocessing import Process
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -25,18 +25,114 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # Global variables
+carddata = []
 commandercolours = []
 manasymbols = {'N': 0, 'C': 0, 'W': 0, 'U': 0, 'B': 0, 'R': 0, 'G': 0}
 landmanasymbols = {'C': 0, 'W': 0, 'U': 0, 'B': 0, 'R': 0, 'G': 0}
 typedist = {'planeswalkers': 0, 'creatures': 0, 'sorceries': 0, 'instants': 0, 'artifacts': 0, 'enchantments': 0,
             'lands': 0}
-# Loads the Scryfall card data from JSON to Python dictionary
-print('Retrieving Card Data...')
-dataform = str("oracle_cards.json").strip("'<>() ").replace('\'', '\"')
-if dataform is None:
-    exit('Error parsing card data')
-with open(dataform, encoding='utf8') as json_file:
-    carddata = json.load(json_file)
+
+
+def main():
+    """
+    Execute the main function, calling various other functions at the user's request
+    """
+    if not os.path.isfile(r"C:\Users\Fred\Desktop\CommanderCraft\decklist.json"):
+        print('Please input your deck URL below:')
+        decklist = process_deck('https://www.moxfield.com/decks/mskhYkY4vUenK3khBTrcMg')
+    else:
+        print('Deck located on file! Retrieving...')
+        dataform = str("decklist.json").strip("'<>() ").replace('\'', '\"')
+        with open(dataform, encoding='utf8') as json_file:
+            decklist = json.load(json_file)
+    if not check_legality(decklist):
+        exit("Deck is illegal, exiting...")
+    # basic_functions(decklist)
+    tagger_functions(decklist)
+
+
+def process_deck(deckinput):
+    """
+    Takes deckinput from user, loads oracle card data and creates a decklist.json out of it, including tags
+    :param deckinput:
+    :return decklist:
+    """
+    # Loads the Scryfall card data from JSON to Python dictionary
+    print('Retrieving Card Data...')
+    dataform = str("oracle_cards.json").strip("'<>() ").replace('\'', '\"')
+    if dataform is None:
+        exit('Error parsing card data')
+    with open(dataform, encoding='utf8') as json_file:
+        carddata = json.load(json_file)
+
+    # Creates a new decklist out of Scryfall data, using the decklist provided
+    print('Processing now...')
+    rawdecklist = mtg_parser.parse_deck(deckinput)
+    decklist = []
+    for card in rawdecklist:
+        i = 0
+        for _ in carddata:
+            if carddata[i].get('name') == card.name:
+                j = 0
+                while j < card.quantity:
+                    decklist.append(carddata[i])
+                    j += 1
+            i += 1
+    card_tags(decklist)
+    print('Writing new decklist txt file...')
+    with open('decklist.json', 'x') as deck_file:
+        deck_file.write(json.dumps(decklist))
+    return decklist
+
+
+def card_tags(decklist):
+    print('Beginning tag search...')
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.experimental_options['prefs'] = {
+        'profile.managed_default_content_settings.images': 2
+    }
+    driver = webdriver.Chrome(options)
+    for card in decklist:
+        if 'Basic Land' not in card['type_line']:
+            url = 'https://tagger.scryfall.com/card/' + card['set'] + '/' + str(card['collector_number'])
+            driver.get(url)
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(),"
+                                                                                        "'artwork')]")))
+            page = driver.page_source
+            soup = BeautifulSoup(page, 'html.parser')
+            container = soup.find_all('div', attrs={'class': 'tag-row'})
+
+            arttags = []
+            functiontags = []
+            for element in container:
+                element = str(element)
+                if element.find('/tags/artwork/') > 0:
+                    arttag = ''
+                    substr = element.find('/tags/artwork/') + 14
+                    for char in element[substr:]:
+                        if char.isalpha() or char.isnumeric() or char == '-' or char == ' ':
+                            arttag = arttag + char
+                        else:
+                            break
+                    arttags.append(arttag)
+                if element.find('/tags/card/') > 0:
+                    functiontag = ''
+                    substr = element.find('/tags/card/') + 11
+                    for char in element[substr:]:
+                        if char.isalpha() or char.isnumeric() or char == '-' or char == ' ':
+                            functiontag = functiontag + char
+                        else:
+                            break
+                    functiontags.append(functiontag)
+            print('Art Tags:', card['name'], arttags)
+            print('Function Tags:', card['name'], functiontags)
+            card['arttags'] = arttags
+            card['functiontags'] = functiontags
+    driver.quit()
 
 
 def check_legality(decklist):
@@ -167,90 +263,30 @@ def basic_functions(decklist):
     print('Average EDHREC rank:', edhrectotal)
 
 
-def process_deck(deckinput):
-    """
-    Takes deckinput from user, returns decklist, a list of card structs
-    :param deckinput:
-    :return decklist:
-    """
-    print('Processing now...')
-    rawdecklist = mtg_parser.parse_deck(deckinput)
-    # Creates a new decklist out of Scryfall data, using the decklist provided
-    decklist = []
-    for card in rawdecklist:
-        i = 0
-        for _ in carddata:
-            if carddata[i].get('name') == card.name:
-                j = 0
-                while j < card.quantity:
-                    decklist.append(carddata[i])
-                    j += 1
-            i += 1
-    return decklist
-
-
-def card_tags(decklist):
-    print('Beginning tag search...')
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.experimental_options['prefs'] = {
-        'profile.managed_default_content_settings.images': 2
-    }
-    driver = webdriver.Chrome(options)
+def tagger_functions(decklist):
+    allarttags = []
+    allfunctags = []
+    removalcards = []
     for card in decklist:
         if 'Basic Land' not in card['type_line']:
-            url = 'https://tagger.scryfall.com/card/' + card['set'] + '/' + str(card['collector_number'])
-            driver.get(url)
-            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(),"
-                                                                                        "'artwork')]")))
-            page = driver.page_source
-            soup = BeautifulSoup(page, 'html.parser')
-            container = soup.find_all('div', attrs={'class': 'tag-row'})
+            allarttags.extend(card['arttags'])
+            allfunctags.extend(card['functiontags'])
+            for tag in card['functiontags']:
+                if 'removal' in tag:
+                    removalcards.append(card)
+                    break
+    commonart = Counter(allarttags)
+    commonfunc = Counter(allfunctags)
+    print(max(commonart, key=commonart.get), 'is the most common art tag (consisting of',
+          max(commonart.values()), 'cards)')
+    print(max(commonfunc, key=commonfunc.get), 'is the most common art tag (consisting of',
+          max(commonfunc.values()), 'cards)')
 
-            arttags = []
-            functiontags = []
-            for element in container:
-                element = str(element)
-                if element.find('/tags/artwork/') > 0:
-                    arttag = ''
-                    substr = element.find('/tags/artwork/') + 14
-                    for char in element[substr:]:
-                        if char.isalpha():
-                            arttag = arttag + char
-                        else:
-                            break
-                    arttags.append(arttag)
-                if element.find('/tags/card/') > 0:
-                    functiontag = ''
-                    substr = element.find('/tags/card/') + 11
-                    for char in element[substr:]:
-                        if char.isalpha() or char == '-':
-                            functiontag = functiontag + char
-                        else:
-                            break
-                    functiontags.append(functiontag)
-            print('Art Tags:', arttags)
-            print('Function Tags:', functiontags)
-    driver.quit()
-
-
-def main():
-    """
-    Execute the main function, calling various other functions at the user's request
-    """
-    print('Please input your deck URL below:')
-    decklist = process_deck('https://www.moxfield.com/decks/mskhYkY4vUenK3khBTrcMg')
-    if not check_legality(decklist):
-        exit("Deck is illegal, exiting...")
-    basic_functions(decklist)
-    p1 = Process(target=card_tags(decklist))
-    p1.start()
-    p2 = Process(target=card_tags(decklist))
-    p2.start()
-    card_tags(decklist)
+    print('You have', len(removalcards), 'pieces of removal:')
+    cardstr = ''
+    for card in removalcards:
+        cardstr = cardstr + '[' + card['name'] + '], '
+    print(cardstr)
 
 
 if __name__ == '__main__':
